2ccda1bebf07ff685693d608315bcca9cdb558ab
Barbara Cunha <barbara.torres.cunha@gmail.com>
2025-07-21 18:39:37 -0300

    Adicionados get e set da classe Vendas

--- src/Corretor.java
+++ src/Corretor.java
@@ -9,6 +9,24 @@ public class Corretor {
     private int id_corretor;
     private Set<Integer> vendas_realizadas = new HashSet<Integer>(); // lista de vendas feitas pelo corretor
 
+    /**Métodos de Get e Set*/
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId_corretor() {
+        return id_corretor;
+    }
+
+    public void setId_corretor(int id_corretor) {
+        this.id_corretor = id_corretor;
+    }
+
     public Set<Integer> getVendas_realizadas() {
         return vendas_realizadas;
     }
@@ -17,6 +35,7 @@ public class Corretor {
         this.vendas_realizadas = vendas_realizadas;
     }
 
+    /** Método para inserir uma venda à lista de vendas do corretor*/
     public void insertVendas_realizadas(Integer id_venda) {
         this.vendas_realizadas.add(id_venda);
     }
