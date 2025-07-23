0392ff25151c27670a95322fad06c04ea508faec
Barbs Ellen <91839395+BarbsII@users.noreply.github.com>
2025-07-23 00:06:31 -0300

    Removido linhas de código teste de Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        Imovel teste1 = new Apartamento();
-        Imovel teste2 = new Casa();
-        Imovel teste3 = new Terreno();
-
-
         SistemaImobiliarioGUI.init();
     }
 }
